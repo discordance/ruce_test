c919a81f295e690aa82cbca600bc125d9fa21f3f
aymeric <aymeric.nunge@gmail.com>
2021-12-20 10:07:24 +0100

    working SVF SIMD

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+#![feature(portable_simd)]
+
+mod svf;
+use core_simd::f32x2;
+use svf::SVFSimper;
+
+use ruce_bindings::ruce::ffi;
+use ruce_bindings::ruce::ruce_types::{PluginProcessor, PluginProcessorImpl};
+pub struct MyProcessorType {
+    lpf: SVFSimper
+}
+
+impl PluginProcessor for MyProcessorType {
+    fn new() -> Self {
+        return MyProcessorType {
+            // default filter
+            lpf: SVFSimper::new(800.0, 0.6, 44100.0)
+        };
+    }
+
+    // JUCE triggers this
+    fn prepare_to_play(&mut self, sample_rate: f32, maximum_expected_samples_per_block: usize) {
+        println!(
+            "{} {} {}",
+            "Rust Audio Processor", sample_rate, maximum_expected_samples_per_block
+        );
+
+        // reset the lpf
+        self.lpf.set(800.0, 0.6, sample_rate);
+    }
+
+    // JUCE calls this function passing a channel interleaved mutable buffer.
+    // This callback runs in the audio thread.
+    fn process_block(&mut self, audio_data: &mut [f32], num_channels: usize, _num_samples: usize) {
+        // process all channels
+        for frame in audio_data.chunks_mut(num_channels) {
+            // assume stereo frame
+            let simd_frame = f32x2::from_slice(frame);
+            let lp = self.lpf.process(simd_frame);
+
+            frame.copy_from_slice(&lp.to_array());
+        }
+    }
+}
+
+ruce_bindings::ruce_vst3::create_processor!(MyProcessorType);
---
+++ src/svf.rs
@@ -0,0 +1,56 @@
+// example copied from baseplug: https://github.com/wrl/baseplug/blob/trunk/examples/svf/svf_simper.rs
+// adapted to work with core_simd
+// implemented from https://cytomic.com/files/dsp/SvfLinearTrapOptimised2.pdf
+// thanks, andy!
+
+use std::f32::consts;
+use core_simd::f32x2;
+
+pub struct SVFSimper {
+    pub a1: f32x2,
+    pub a2: f32x2,
+    pub a3: f32x2,
+
+    pub ic1eq: f32x2,
+    pub ic2eq: f32x2
+}
+
+impl SVFSimper {
+    pub fn new(cutoff: f32, resonance: f32, sample_rate: f32) -> Self {
+        let g = (consts::PI * (cutoff / sample_rate)).tan();
+        let k = 2f32 - (1.9f32 * resonance.min(1f32).max(0f32));
+
+        let a1 = 1.0 / (1.0 + (g * (g + k)));
+        let a2 = g * a1;
+        let a3 = g * a2;
+
+        SVFSimper {
+            a1: f32x2::splat(a1),
+            a2: f32x2::splat(a2),
+            a3: f32x2::splat(a3),
+
+            ic1eq: f32x2::splat(0.0),
+            ic2eq: f32x2::splat(0.0)
+        }
+    }
+
+    pub fn set(&mut self, cutoff: f32, resonance: f32, sample_rate: f32) {
+        let new = Self::new(cutoff, resonance, sample_rate);
+
+        self.a1 = new.a1;
+        self.a2 = new.a2;
+        self.a3 = new.a3;
+    }
+
+    #[inline]
+    pub fn process(&mut self, v0: f32x2) -> f32x2 {
+        let v3 = v0 - self.ic2eq;
+        let v1 = (self.a1 * self.ic1eq) + (self.a2 * v3);
+        let v2 = self.ic2eq + (self.a2 * self.ic1eq) + (self.a3 * v3);
+
+        self.ic1eq = (f32x2::splat(2.0) * v1) - self.ic1eq;
+        self.ic2eq = (f32x2::splat(2.0) * v2) - self.ic2eq;
+
+        v2
+    }
+}
